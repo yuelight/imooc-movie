8b22b0e446968da4df7225f2d70994d92a525d42
yuelight <vaeqfe@gmail.com>
2017-01-20 16:22:08 +0800

    fix indent

--- config/config.js
+++ config/config.js
@@ -1,34 +1,34 @@
 var path = require('path'),
-    rootPath = path.normalize(__dirname + '/..'),
-    env = process.env.NODE_ENV || 'development';
+        rootPath = path.normalize(__dirname + '/..'),
+        env = process.env.NODE_ENV || 'development';
 
 var config = {
-  development: {
-    root: rootPath,
-    app: {
-      name: 'imooc-movie'
+    development: {
+        root: rootPath,
+        app: {
+            name: 'imooc-movie'
+        },
+        port: process.env.PORT || 3000,
+        db: 'mongodb://localhost/imooc-movie-development'
     },
-    port: process.env.PORT || 3000,
-    db: 'mongodb://localhost/imooc-movie-development'
-  },
 
-  test: {
-    root: rootPath,
-    app: {
-      name: 'imooc-movie'
+    test: {
+        root: rootPath,
+        app: {
+            name: 'imooc-movie'
+        },
+        port: process.env.PORT || 3000,
+        db: 'mongodb://localhost/imooc-movie-test'
     },
-    port: process.env.PORT || 3000,
-    db: 'mongodb://localhost/imooc-movie-test'
-  },
 
-  production: {
-    root: rootPath,
-    app: {
-      name: 'imooc-movie'
-    },
-    port: process.env.PORT || 3000,
-    db: 'mongodb://localhost/imooc-movie-production'
-  }
+    production: {
+        root: rootPath,
+        app: {
+            name: 'imooc-movie'
+        },
+        port: process.env.PORT || 3000,
+        db: 'mongodb://localhost/imooc-movie-production'
+    }
 };
 
 module.exports = config[env];
