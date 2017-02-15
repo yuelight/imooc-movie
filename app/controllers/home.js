41e1bdab32de531feaa8480d214048d4c1242dd4
yuelight <vaeqfe@gmail.com>
2017-02-15 20:12:13 +0800

    divide data into many pages

--- app/controllers/home.js
+++ app/controllers/home.js
@@ -63,3 +63,53 @@ router.get('/movie/:id', function(req, res, next) {
             })
     });
 });
+
+// search
+router.get('/results', function (req, res, next) {
+    var catId = req.query.cat;
+    var page = parseInt(req.query.p, 10) || 0;
+    var q = req.query.q;
+    var count = 2;
+    var index = page * count;
+
+    if (catId) {
+        Category.find({_id: catId}).populate({
+            path: 'movies',
+            select: 'title poster'
+        }).exec(function(err, categories) {
+            if (err)
+            throw err;
+
+            var category = categories[0] || {};
+            var movies = category.movies || {};
+            var results = movies.slice(index, index + count);
+
+            res.render('pages/result', {
+                title: 'imooc 结果列表页',
+                keyword: category.name,
+                currentPage: (page + 1),
+                query: 'cat=' + catId,
+                totalPage: Math.ceil(movies.length / count),
+                movies: results
+            });
+        });
+    } else {
+        Movie
+            .find({title: new RegExp(q + '.*', 'i')})
+            .exec(function (err, movies) {
+                if (err)
+                throw err;
+
+                var results = movies.slice(index, index + count);
+
+                res.render('pages/result', {
+                    title: 'imooc 结果列表页',
+                    keyword: q,
+                    currentPage: (page + 1),
+                    query: 'q=' + q,
+                    totalPage: Math.ceil(movies.length / count),
+                    movies: results
+                });
+            });
+    }
+});
